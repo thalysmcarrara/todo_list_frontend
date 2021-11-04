539d62892c00449810abc39c9605aa5acff186e5
thalysmcarrara <thalysmatiascarrara@gmail.com>
2021-11-04 16:05:08 -0400

    :sparkles: feat - implemented Home navigation behavior -> Login by clicking the login button

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import './index.css';
 
 function Header() {
+  const history = useHistory();
+
+  const navigateToLogin = () => {
+    history.push('/login');
+  };
+
   return (
     <header>
       <h1>
         Todo List
       </h1>
-      <button data-testid="login-btn" type="button" className="button-login">
+      <button
+        onClick={ navigateToLogin }
+        data-testid="login-btn"
+        type="button"
+        className="button-login"
+      >
         Login
         <span className="material-icons">
           login
